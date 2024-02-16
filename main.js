f124908f9b6847c3f9fe7067c5febd592c40defd
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-02-16 15:54:50 -0500

    Create Grid Reset Button with Function
    this commit adds a button that allows the user
    to reset the board and choose how many pixels
    they want as the width of the board.

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("button")
 
-let j = 10;
+btn.addEventListener('click', resetGrid);
 
-for (i = 0; i < j; i++) {
+createGrid(16);
+
+function createGrid(size) {
+for (i = 0; i < size; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     container.appendChild(row);
-        for (g = 0; g < j; g++) {
+        for (g = 0; g < size; g++) {
             const pixel = document.createElement("div");
             pixel.classList.add("pixel");
-            row.appendChild(pixel);
-        }
+            row.appendChild(pixel);}}
+
+    const pixels = document.querySelectorAll(".pixel")
+    pixels.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+    div.style.cssText = "background-color: black;";
+    })})}
+
+function resetGrid() {
+    let rows = document.querySelectorAll(".row")
+    rows.forEach ((row) => {
+        row.remove()})
+        
+    let num;
+    do {num = prompt("Enter a number greater than 100?");
+    } while (num > 100);
+
+    createGrid(num);
 }
